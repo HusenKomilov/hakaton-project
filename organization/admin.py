24a4f0742029f5cfffc09636bb1fb6f6c93ac792
HusenKomilov <komilovhusen73@gmail.com>
2024-05-23 01:35:27 +0500

    feat: all model register in admin.py and update admin panel design

--- organization/admin.py
+++ organization/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from .models import Organization
 # Register your models here.
 
+
+@admin.register(Organization)
 class OrganizationAdmin(admin.ModelAdmin):
     list_display = ['organization_name']
     search_fields = ['organization_name', "total_money"]
-admin.site.register(Organization, OrganizationAdmin)
